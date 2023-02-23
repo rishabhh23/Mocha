8123fb9dd8aff845f4780bdb9c6e94f6d67259a5
Rishabh Sharma <rish23.sharma@gmail.com>
2023-02-23 23:00:43 +0530

    Connecting to blockchain

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,16 @@
-require("@nomicfoundation/hardhat-toolbox");
+import * as dotenv from "dotenv";
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.17",
+dotenv.config();
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+
+const HardhatUserConfig = {
+  networks: {
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [
+        "eccc28c7eb9e8a099996fc71eab170589413525518199712d3e8b29b07badf7c",
+      ],
+    },
+  },
 };
--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -1,8 +1,12 @@
 const main = async () => {
-  const Transactions = await hre.ethers.getContractFactory("Transactions");
-  const transactions = await Transactions.deploy();
-  await transactions.deployed();
-  console.log("Transactions deployed to: ", transactions.address);
+  const transactionsFactory = await hre.ethers.getContractFactory(
+    "Transactions"
+  );
+  const transactionsContract = await transactionsFactory.deploy();
+
+  await transactionsContract.deployed();
+
+  console.log("Transactions address: ", transactionsContract.address);
 };
 
 const runMain = async () => {
